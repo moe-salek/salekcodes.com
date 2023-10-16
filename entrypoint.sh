462cc578baf9387dfaf27256b23273451e4b43d6
mohammadsalek <salek.mohmd@gmail.com>
2023-10-16 21:30:20 +0200

    [FEAT] entrypoint runs daphne

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
-# TODO: Add django startup script
+python manage.py collectstatic --noinput &&
+    python manage.py makemigrations core &&
+    python manage.py migrate
+
+daphne -b 0.0.0.0 -p 8000 salekcodes.asgi:application
