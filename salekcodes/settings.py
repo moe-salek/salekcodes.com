4338f1e42047a1a7ba01f8b4c9a71c5230e61ecb
mohammadsalek <salek.mohmd@gmail.com>
2023-10-16 21:47:27 +0200

    [FEAT] add whitenoise

--- salekcodes/settings.py
+++ salekcodes/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from decouple import config
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,6 +93,6 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
