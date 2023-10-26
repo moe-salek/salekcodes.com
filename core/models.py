94460d9e9065b73f4c598266dfb513cb1a6ffafb
Mohammad Salek <25065446+MohammadSalek@users.noreply.github.com>
2023-10-26 22:55:00 +0200

    [FIX] superuser create method

--- core/models.py
+++ core/models.py
@@ -18,22 +18,20 @@ class UserManager(BaseUserManager):
         # create user:
         normalized_email = self.normalize_email(email)
         user = self.model(email=normalized_email, **extra_fields)
-        user.set_password(password)  # type: ignore[attr-defined]
-        user.save()
+        user.set_password(password)
+        user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self: Self, email: str, password: str, **extra_fields: dict[str, Any]):
+        # create normal user:
+        user = self.create_user(email, password, **extra_fields)
+        # make it superuser:
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
