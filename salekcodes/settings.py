4d2d0b11e2ff020e81fe2169694c3d5dcf29b48f
Mohammad Salek <25065446+MohammadSalek@users.noreply.github.com>
2023-10-28 21:11:54 +0200

    [FEAT] nginx serve

--- salekcodes/settings.py
+++ salekcodes/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from decouple import config
@@ -93,8 +92,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
