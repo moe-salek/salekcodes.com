9d78c527fc236031eb35e8346918a2389e36e594
Mohammad Salek <25065446+MohammadSalek@users.noreply.github.com>
2023-11-23 21:16:20 +0100

    [FEAT] init resume serializers

---
+++ resume_cv/serializers.py
@@ -0,0 +1,40 @@
+# from rest_framework import serializers
+
+# from resume_cv.models import Skill, Experience, Education, Certificate, Award, Contact, ResumeCV
+
+
+# class SkillSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Skill
+#         fields = ['id', 'name', 'description']
+#         read_only_fields = fields
+
+
+# class ExperienceSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Experience
+
+
+# class EducationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Education
+
+
+# class CertificateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Certificate
+
+
+# class AwardSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Award
+
+
+# class ContactSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Contact
+
+
+# class ResumeCVSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ResumeCV
