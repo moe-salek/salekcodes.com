c3e7568c844ccec3aceaa4808c15230e000ed659
Mohammad Salek <25065446+MohammadSalek@users.noreply.github.com>
2024-03-23 10:03:21 +0330

    [REF] rem frontend

--- frontend/salekcodes-vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import StaticPage from './components/StaticPage.vue'
-</script>
-
-<template>
-  <StaticPage class="bg-gradient-to-t from-indigo-600 to-indigo-900 font-mono" />
-</template>
-
-<style scoped></style>
