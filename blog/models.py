766154a7cc42af0cbf2f70e5bc28a0497111715e
Mohammad Salek <25065446+MohammadSalek@users.noreply.github.com>
2024-06-07 22:34:18 +0330

    [REF] add base model

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,26 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
 from django_quill.fields import QuillField
+from django.utils import timezone
 
-from salekcodes.models import Base
+# region base
+
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
+
+class Base(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    modified_at = AutoDateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+# endregion
 
 
 class Post(Base):
