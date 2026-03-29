d395e2dee6cf4ed5ea2bf0934c04c7f0f3a2b0a7
Moe Salek <25065446+moe-salek@users.noreply.github.com>
2026-03-29 15:52:47 +0330

    ref: ren static root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}", "./staticfiles/**/*.{html,js}", "./static/**/*.{html,js}"],
+  content: ["./templates/**/*.{html,js}", "./static/**/*.{html,js}"],
   extend: {
     minHeight: {
       'screen': '100vh',
@@ -8,4 +8,3 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography'),],
 }
-
