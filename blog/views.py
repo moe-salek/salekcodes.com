b286bc90d0f3c5ee710faf785e327cde88f298ce
Moe Salek <25065446+moe-salek@users.noreply.github.com>
2026-07-10 16:46:34 +0330

    feat: seo meta tags and rss footer
    Add meta/OG descriptions with per-echo values, article OG tags on
    echo pages, favicon and feed links in the head, and a site footer
    with the RSS link.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
 
 from blog.models import Post
+from blog.templatetags.markdown_utils import markdown_to_html
 
 ECHOES_PAGE_SIZE = 10
 
@@ -21,7 +24,8 @@ def echoes_archive(request):
 
 def echo_unique_page(request, slug: str):
     post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLISHED)
-    ctx = {'echo_post': post}
+    meta_description = Truncator(strip_tags(markdown_to_html(post.content))).chars(160)
+    ctx = {'echo_post': post, 'meta_description': meta_description}
     return render(request, 'blog/echo_unique_page.html', ctx)
 
 
